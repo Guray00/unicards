c555bc61ee5842a3d815beae77727b4409a13c42
Marco <30523962+Guray00@users.noreply.github.com>
2021-04-05 00:46:23 +0200

    aggiunto font handling

--- pages/login.php
+++ pages/login.php
@@ -13,13 +13,20 @@
 
 
 <html>
-	<body>
-		<h1>Login</h1>
 
+	<header>
+		<title>unicards</title>
+		<link rel="stylesheet" href="../css/theme.css">
+		<link rel="stylesheet" href="../css/login.css">
+	</header>
+
+
+	<body>
 		<form id="login" method="POST" action="../php/login.php">
-					
-			<label>Mail:</label> <input type "text" name="mail" required> <br>
-			<label>Password:</label> <input type "text" name="password" required> <br>
+			<h1>Login</h1>
+
+			<label>Mail:</label> <input type="email" name="mail" required> <br>
+			<label>Password:</label> <input type="password" name="password" required> <br>
 		
 			<input type="submit" value="Login" name="login" method="POST">
 		</form>
