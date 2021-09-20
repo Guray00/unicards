71190e28df937690052877e17396de284cbf05e5
Marco <30523962+Guray00@users.noreply.github.com>
2021-09-20 23:09:53 +0200

    modifiche minime

--- js/landing.js
+++ js/landing.js
@@ -7,6 +7,7 @@ function slideBackground() {
 	var bg2 = document.getElementById("flashcards");
 	var bg5 = document.getElementById("tuo-mazzo");
 	var bg3 = document.getElementById("amici");
+	var bg6 = document.getElementById("opensource");
 
 	var x = 0;
 	
@@ -14,8 +15,10 @@ function slideBackground() {
 		bg1.style.backgroundPosition = x + 'px ' + x+"px";
 		bg2.style.backgroundPosition = x + 'px ' + x+"px";
 		bg3.style.backgroundPosition = x + 'px ' + x+"px";
-		bg4.style.backgroundPosition = (x*(-0.3)) + 'px ' +(x*(-0.3)) +"px";
+		bg4.style.backgroundPosition = x + 'px ' + x+"px";
 		bg5.style.backgroundPosition = x + 'px ' + x+"px";
+		bg6.style.backgroundPosition = x + 'px ' + x+"px";
+
 		x-=0.4;
 	}, 40);
 	
@@ -23,3 +26,4 @@ function slideBackground() {
 
 
 window.addEventListener('load',slideBackground,false);
+//bg4.style.backgroundPosition = (x*(-0.3)) + 'px ' +(x*(-0.3)) +"px";
